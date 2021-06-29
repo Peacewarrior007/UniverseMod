9a97e76aec19e46c57a590e1d562b921a6442ef3
aqts-aqts <alvinxiao07@gmail.com>
2021-06-29 09:43:05 -0700

    Custom gravity

---
+++ src/main/java/net/mcreator/universemod/procedures/GravityProcedure.java
@@ -0,0 +1,21 @@
+package net.mcreator.universemod.procedures;
+
+import net.mcreator.universemod.UniverseModModElements;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+@UniverseModModElements.ModElement.Tag
+public class GravityProcedure extends UniverseModModElements.ModElement {
+	public GravityProcedure(UniverseModModElements instance) {
+		super(instance, 4);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies, Entity entity, double power) {
+		double velocity = 0;
+		if ((((velocity) + (power)) < 4)) {
+			velocity = (double) (((velocity) + (power / 10)));
+			entity.setMotion(entity.getMotion().getX(), (entity.getMotion().getY() - velocity), entity.getMotion().getZ());
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/universemod/procedures/OnPlayerTickEventProcedure.java
@@ -0,0 +1,58 @@
+package net.mcreator.universemod.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.universemod.UniverseModModElements;
+import net.mcreator.universemod.UniverseModMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@UniverseModModElements.ModElement.Tag
+public class OnPlayerTickEventProcedure extends UniverseModModElements.ModElement {
+	public OnPlayerTickEventProcedure(UniverseModModElements instance) {
+		super(instance, 2);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				UniverseModMod.LOGGER.warn("Failed to load dependency entity for procedure OnPlayerTickEvent!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		entity.fallDistance = (float) (0);
+		entity.setNoGravity((true));
+		{
+			if (!entity.isOnGround()) {
+				Map<String, Object> $_dependencies = new HashMap<>();
+				GravityProcedure.executeProcedure($_dependencies, entity, 1);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			Entity entity = event.player;
+			World world = entity.world;
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
